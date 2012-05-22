782bf6911df83e9fe3370544c2a3c23161a50f95
yukio-tani <yukio-tani@garage.co.jp>
2012-05-22 18:02:52 +0900

    implement dynamic mypage

--- app/Controller/MypageController.php
+++ app/Controller/MypageController.php
@@ -1,10 +1,14 @@
 <?php
 App::uses('Crypt', 'euonymus');
 class MypageController extends AppController {
+  public $uses = array('Album');
+
   function index() {
     if(!$this->OauthLogin->login){
       $this->redirect('/login?location=' . Crypt::encrypt('/album/create'));
     }
+    $albums = $this->Album->getAlbumsById($this->OauthLogin->tw_user['id'], $limit = 7);
+    $this->set('albums', $albums);
     $this->set('tw_user', $this->OauthLogin->tw_user);
   }
 }
