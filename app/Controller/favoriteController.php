f37b5d1e6bccc8be689f2910f934adaec0c2ac07
yukio-tani <yukio-tani@garage.co.jp>
2012-05-23 22:08:14 +0900

    add favorite

--- app/Controller/favoriteController.php
+++ app/Controller/favoriteController.php
@@ -9,4 +9,29 @@ class FavoriteController extends AppController {
 
     $this->set('albums', $albums);
   }
+
+  function add($id) {
+    if (is_null($id)) $this->redirect('/');
+    $this->loginCheck();
+    
+    //TODO ‚¨‹C‚É“ü‚è‚É’Ç‰Á
+    echo "under construction<br />";
+    $albums = $this->Favorite->getFavorites($this->OauthLogin->tw_user['id']);
+
+    $this->set('albums', $albums);
+    $this->render('index');
+  }
+
+  function del($id) {
+    if (is_null($id)) $this->redirect('/');
+    $this->loginCheck();
+    
+    //TODO ‚¨‹C‚É“ü‚è‚©‚çíœ
+    echo "under construction<br />";
+    $albums = $this->Favorite->getFavorites($this->OauthLogin->tw_user['id']);
+
+    $this->set('albums', $albums);
+    $this->render('index');
+  }
+
 }
